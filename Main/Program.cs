8f90be67fe84cba9fd1e648e1d9358d6d1fca611
NaClnik <danycall19@gmail.com>
2022-10-24 23:08:24 +0300

    add

--- Main/Program.cs
+++ Main/Program.cs
@@ -8,10 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers(options =>
-{
-    options.Conventions.Add(new RouteTokenTransformerConvention(new KebabCaseParameterTransformer()));
-});
+builder.Services.AddControllers();
+// builder.Services.AddControllers(options =>
+// {
+//     options.Conventions.Add(new RouteTokenTransformerConvention(new KebabCaseParameterTransformer()));
+// });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
