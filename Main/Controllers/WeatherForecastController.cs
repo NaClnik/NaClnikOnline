ed00d7087cbf47760de6c64401091eaebf882ccf
NaClnik <danycall19@gmail.com>
2022-10-24 17:25:04 +0300

    add

--- Main/Controllers/WeatherForecastController.cs
+++ Main/Controllers/WeatherForecastController.cs
@@ -39,6 +39,6 @@ public class WeatherForecastController : ControllerBase
     [HttpGet]
     public ActionResult<string> Sasa()
     {
-        return "Остановись!";
+        return "Lady dadada!";
     }
 }
